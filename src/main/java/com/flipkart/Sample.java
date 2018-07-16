2742e02e133e0abe9393b43eedf4d3f2f6763b99
Manchala <manchala@manchala.com>
2018-07-16 07:03:05 +0000

    BUg fixes at line number 6

--- src/main/java/com/flipkart/Sample.java
+++ src/main/java/com/flipkart/Sample.java
@@ -3,7 +3,7 @@ public class Sample
 	public static void main(String args[])
 	{       
 		int n=10;
-		for (i=1;i++;i<=n)
+		for (int i=1; i<=n; i++)
 		{	
 			System.out.println("The value is:"+i);
 		}
